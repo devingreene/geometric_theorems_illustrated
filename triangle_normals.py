f1f25321caab5fc5335e3c62d529f5a22422e108
devingreene <devin@greene.cz>
2017-03-16 20:08:53 -0400

    Normal failed to extend over whole plot when one of sides small. Fixed it.

--- triangle_normals.py
+++ triangle_normals.py
@@ -1,6 +1,8 @@
 from matplotlib.pyplot import *
 import matplotlib.backend_bases as be
 import numpy as np 
+from numpy import sqrt
+
 
 # Mouse movement functions
 def reg_down(evt):
@@ -37,8 +39,20 @@ def redefine_triangle():
 	n[2][0].set_data(nx[4:],ny[4:])
 	fig.show()
 
+def define_ex_factor():
+	# Insures that normal lines will extend over 
+	# plot
+	global E
+	ds = ( sqrt((x[0]-x[1])**2 + (y[0]-y[1])**2),\
+			sqrt((x[0]-x[2])**2 + (y[0]-y[2])**2),\
+			sqrt((x[1]-x[2])**2 + (y[1]-y[2])**2) )
+	E = 5/min(ds)
+
 def draw_normal_lines():
 	global nx,ny
+
+	define_ex_factor()
+
 	nx = (
 		x[0] +  E*(y[1]-y[2]),
 		x[0] -  E*(y[1]-y[2]),
@@ -67,9 +81,10 @@ down_state = 0
 x = [-1,0.5,0.5]
 y = [0,-0.5*np.sqrt(3),0.5*np.sqrt(3)]
 
+define_ex_factor()
+
 p = plot(x[0],y[0]),plot(x[1],y[1]),plot(x[2],y[2])
 l = plot(x[:2],y[:2],'y'),plot(x[1:],y[1:],'y'),plot([x[2],x[0]],[y[2],y[0]],'y')
-E = 10
 draw_normal_lines()
 n = plot(nx[0:2],ny[0:2]),plot(nx[2:4],ny[2:4]),plot(nx[4:],ny[4:])
 
